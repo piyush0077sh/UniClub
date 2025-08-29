781ea91db379755f92d3cfc6a33a0b5a0366eb72
Piyush Singh <139443914+piyush0077sh@users.noreply.github.com>
2025-08-30 00:07:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["gkkr3x-5173.csb.app"] // allow all Codesandbox subdomains
+    allowedHosts: [
+      'tc78pq-5173.csb.app'  // 👈 add the sandbox host shown in your error
+    ]
   }
-});
+})
